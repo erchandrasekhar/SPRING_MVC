075f1f4b7a5a7d7434318d2d551c5f948aa305cf
chandrasekhar yadav <er.chandrasekharyadav@gmail.com>
2019-03-15 16:18:30 +0530

    Model created

---
+++ springmvclogin/src/main/java/com/springmvclogin/models/User.java
@@ -0,0 +1,67 @@
+package com.springmvclogin.models;
+
+public class User {
+	
+	private Integer id;
+	private String userName;
+	private String email;
+	private String password;
+	private String confirmPassword;
+	
+	/*Constructor deleration*/
+	
+	public User(Integer id, String userName, String email, String password, String confirmPassword) {
+		super();
+		this.id = id;
+		this.userName = userName;
+		this.email = email;
+		this.password = password;
+		this.confirmPassword = confirmPassword;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getConfirmPassword() {
+		return confirmPassword;
+	}
+
+	public void setConfirmPassword(String confirmPassword) {
+		this.confirmPassword = confirmPassword;
+	}
+	
+	/*getter and setter*/
+	
+	
+	
+
+}
